d4366c6ff0de740de3bf12d0b3dace7d0c5314c7
PrimmR <93214758+PrimmR@users.noreply.github.com>
2023-10-01 16:08:32 +0000

    Implement SHA1 Hash

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 extern crate chrono;
 extern crate serde;
-extern crate sha1;
 
 use serde::{Deserialize, Serialize};
 
@@ -16,15 +15,193 @@ impl Key {
     }
 }
 
-mod hash {
-    pub fn sha1(key: &[u8]) -> Vec<u8> {
-        use sha1::{Digest, Sha1};
+pub mod hash {
+    use std::convert::TryInto;
 
-        let mut hasher = Sha1::new();
-        hasher.update(key);
-        let result = hasher.finalize();
+    #[derive(Debug)]
+    struct SHA1Hash(u32, u32, u32, u32, u32);
 
-        result[..].to_vec()
+    impl SHA1Hash {
+        const H0: u32 = 0x67452301;
+        const H1: u32 = 0xEFCDAB89;
+        const H2: u32 = 0x98BADCFE;
+        const H3: u32 = 0x10325476;
+        const H4: u32 = 0xC3D2E1F0;
+
+        fn new() -> Self {
+            Self(Self::H0, Self::H1, Self::H2, Self::H3, Self::H4)
+        }
+
+        fn to_vec(&self) -> Vec<u8> {
+            let mut v = Vec::new();
+            v.append(&mut self.0.to_be_bytes().to_vec());
+            v.append(&mut self.1.to_be_bytes().to_vec());
+            v.append(&mut self.2.to_be_bytes().to_vec());
+            v.append(&mut self.3.to_be_bytes().to_vec());
+            v.append(&mut self.4.to_be_bytes().to_vec());
+            v
+        }
+    }
+
+    impl std::ops::Add for SHA1Hash {
+        type Output = Self;
+
+        fn add(self, rhs: Self) -> Self::Output {
+            // Prevents overflows
+            Self(
+                self.0.wrapping_add(rhs.0),
+                self.1.wrapping_add(rhs.1),
+                self.2.wrapping_add(rhs.2),
+                self.3.wrapping_add(rhs.3),
+                self.4.wrapping_add(rhs.4),
+            )
+        }
+    }
+
+    pub fn sha1(message: &[u8]) -> Vec<u8> {
+        // Message length in bits
+        let ml: u64 = TryInto::<u64>::try_into(message.len()).unwrap() * 8;
+        let mut message = message.to_vec();
+
+        // Pre-processing
+        message.push(0x80);
+
+        // message len needs to be multiple of (512-64)/8 = 56
+        message = pad_mult(message, 64, 8);
+        message.append(&mut u64::to_be_bytes(ml).to_vec());
+
+        // chunk into 512/8= 64 byte chunks
+        let chunks = message.chunks(64);
+
+        let hash = chunks.fold(SHA1Hash::new(), |acc, x| process_chunks(&acc, x) + acc);
+
+        hash.to_vec()
+    }
+
+    fn process_chunks(hash: &SHA1Hash, chunk: &[u8]) -> SHA1Hash {
+        // Convert 64 byte chunks to 16 32-bit big-endian words
+        let mut words: Vec<u32> = chunk
+            .chunks(4)
+            .map(|x| u32::from_be_bytes(x.try_into().unwrap()))
+            .collect();
+
+        // Creates 80 long vec
+        for i in 16..80 {
+            let item = words[i - 3] ^ words[i - 8] ^ words[i - 14] ^ words[i - 16];
+            words.push(left_rot(item, 1));
+        }
+
+        // Init values
+        let mut a = hash.0;
+        let mut b = hash.1;
+        let mut c = hash.2;
+        let mut d = hash.3;
+        let mut e = hash.4;
+
+        for i in 0..80 {
+            let (f, k): (u32, u32) = match i {
+                0..=19 => (d ^ (b & (c ^ d)), 0x5A827999), // ((b & c) | (!b & d), 0xfa827999),
+                20..=39 => (b ^ c ^ d, 0x6ed9eba1),
+                40..=59 => ((b & c) | (b & d) | (c & d), 0x8f1bbcdc),
+                _ => (b ^ c ^ d, 0xca62c1d6), // 60..=79
+            };
+
+            // Wrapping add keeps number as u32
+            let temp = left_rot(a, 5)
+                .wrapping_add(f)
+                .wrapping_add(e)
+                .wrapping_add(k)
+                .wrapping_add(words[i]);
+
+            e = d;
+            d = c;
+            c = left_rot(b, 30);
+            b = a;
+            a = temp;
+        }
+
+        SHA1Hash(a, b, c, d, e)
+    }
+
+    // Circular left shift
+    fn left_rot(num: u32, by: u8) -> u32 {
+        (num << by) | (num >> (32 - by))
+    }
+
+    fn pad_mult(message: Vec<u8>, mult: usize, sub: usize) -> Vec<u8> {
+        let message_len = message.len();
+
+        let size = (((message_len + sub + mult - 1) / mult) * mult) - sub;
+
+        let mut pad = message;
+        pad.resize(size, 0);
+        pad
+    }
+
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn rot0() {
+            assert_eq!(
+                left_rot(0b00001110_00001110_00001110_00001110, 2),
+                0b00111000_00111000_00111000_00111000
+            )
+        }
+
+        #[test]
+        fn rot1() {
+            assert_eq!(
+                left_rot(0b10111001_10111001_10111001_10111001, 4),
+                0b10011011_10011011_10011011_10011011
+            )
+        }
+
+        #[test]
+        fn pad() {
+            assert_eq!(
+                pad_mult(vec![20, 82, 21, 05], 3, 1),
+                vec![20, 82, 21, 05, 0]
+            )
+        }
+
+        #[test]
+        fn pad_overflow() {
+            assert_eq!(
+                pad_mult(vec![20, 82, 21, 05, 22, 40, 34, 15], 3, 2),
+                vec![20, 82, 21, 05, 22, 40, 34, 15, 0, 0]
+            )
+        }
+
+        #[test]
+        fn sha1_empty() {
+            let key = b"";
+            let result = vec![
+                0xda, 0x39, 0xa3, 0xee, 0x5e, 0x6b, 0x4b, 0x0d, 0x32, 0x55, 0xbf, 0xef, 0x95, 0x60,
+                0x18, 0x90, 0xaf, 0xd8, 0x07, 0x09,
+            ];
+            assert_eq!(sha1(key), result)
+        }
+
+        #[test]
+        fn sha1_single_chunk() {
+            let key = b"Primm";
+            let result = vec![
+                0x59, 0x07, 0x84, 0x5c, 0xeb, 0x72, 0x05, 0x8d, 0xa5, 0x36, 0xa6, 0x23, 0xa0, 0x83,
+                0x8c, 0x5c, 0x1b, 0x92, 0x57, 0xe0,
+            ];
+            assert_eq!(sha1(key), result)
+        }
+
+        #[test]
+        fn sha1_mult_chunk() {
+            let key = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890";
+            let result = vec![
+                0xd2, 0x6c, 0xf5, 0xf8, 0x56, 0xae, 0xaa, 0x77, 0xa7, 0xfb, 0xaa, 0x32, 0x6f, 0x7d,
+                0x31, 0x2c, 0xba, 0xb5, 0xaa, 0x4b,
+            ];
+            assert_eq!(sha1(key), result)
+        }
     }
 }
 
