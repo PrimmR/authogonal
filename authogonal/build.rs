9f74fd258dadf00d1bc3466a54a81acfd98073ba
PrimmR <primmr@icloud.com>
2024-07-07 14:35:59 +0100

    Remove Windows dependencies on non-Windows platforms

--- authogonal/build.rs
+++ authogonal/build.rs
@@ -1,9 +1,11 @@
 use std::env;
-use winresource::WindowsResource;
+
 
 // Applies app icon to Windows release
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     if env::var_os("CARGO_CFG_WINDOWS").is_some() {
+        use winresource::WindowsResource;
+        
         WindowsResource::new()
             .set_icon("../icon/Icon.ico")
             .compile()?;
